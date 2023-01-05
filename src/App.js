056e3a45a02f2a3dac0d0511abcf73102d4d8286
Emeka-12 <nwafornnaemeka34@gmail.com>
2023-01-04 19:53:31 -0700

    update on routes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,21 @@ import NftCards from './components/cards/NftCards';
 import Hero from './components/main/Hero';
 
 
+// PAGES
+
+import BuyNow from './Pages/Buy Now/BuyNow';
+import Connect from './Pages/Connect/Connect';
+import GetStarted from './Pages/Get Started/GetStarted';
+import Team from './Pages/Team/Team';
+import Benefits from './Pages/Benefits/Benefits';
+import HowItWorks from './Pages/How It Works/HowItWorks';
+import BambaNft from './Pages/Bamba Nft/BambaNft';
+
+
+
+
+
+
 
 function App() {
   return (
@@ -16,9 +31,23 @@ function App() {
         </div>
         
           <Routes>
-            <Route path='/' element= {<Hero />} exact/>
+             <Route path='/' element= {<Hero />} exact/>
              <Route path='/nft-cards' element= {<NftCards />}/>
+
+
+              {/* Routes to Created  Pages */}
+
+             <Route path='/bamba-nft' element= {<BambaNft />}/>
+             <Route path='/how-it-works' element= {<HowItWorks />}/>
+             <Route path='/benefits' element= {<Benefits />}/> 
+             <Route path='/team' element= {<Team />}/> 
+
+             <Route path='/get-started' element= {<GetStarted />}/>
+             <Route path='/connect'  element = {<Connect />}/>
+             <Route path='/buy-now' element = {<BuyNow />}/>
              
+
+
           </Routes>
         
      </BrowserRouter>
---
+++ src/Pages/Bamba Nft/BambaNft.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BambaNft = () => {
+  return (
+    <div>
+      Bamba Nft
+    </div>
+  )
+}
+
+export default BambaNft
